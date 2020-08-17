ac5d9207ed4535ae400daae4ca0e60b79b1f597e
Hamidreza Seifi <hamidreza.seifi@mediqon.de>
2020-08-17 14:48:29 +0200

    initial

--- iflow/profile/src/test/groovy/com/pth/profile/test/ProfileTestDataProvider.groovy
+++ iflow/profile/src/test/groovy/com/pth/profile/test/ProfileTestDataProvider.groovy
@@ -3,8 +3,10 @@ package com.pth.profile.test
 import com.pth.common.edo.enums.ECompanyType
 import com.pth.profile.entities.CompanyEntity
 import com.pth.profile.entities.DepartmentEntity
+import com.pth.profile.entities.UserEntity
 import com.pth.profile.repositories.ICompanyRepository
 import com.pth.profile.repositories.IDepartmentRepository
+import com.pth.profile.repositories.IUserRepository
 import com.pth.profile.repositories.impl.CompanyRepository
 import com.pth.profile.repositories.impl.UserRepository
 import org.apache.commons.lang3.RandomStringUtils
@@ -19,14 +21,20 @@ import static java.lang.StringBuilder.*;
 class ProfileTestDataProvider extends Specification {
 
     protected UUID testCompanyId = UUID.fromString("c7ada239-26a7-4402-ad12-ceae751fa7d8")
+
     protected UUID testDepartmentId1 = UUID.fromString("c7ada239-26a7-4402-ad12-ceae751fa7da")
     protected UUID testDepartmentId2 = UUID.fromString("c7ada239-26a7-4402-ad12-ceae751fa7db")
     protected UUID testDepartmentId3 = UUID.fromString("c7ada239-26a7-4402-ad12-ceae751fa7dc")
 
+    protected UUID testUserId1 = UUID.fromString("c7ada239-26a7-4402-ad12-ceae751fa711")
+
     protected CompanyEntity testCompany = null
+
     protected DepartmentEntity testDepartment1 = null
     protected DepartmentEntity testDepartment2 = null
     protected DepartmentEntity testDepartment3 = null
+
+    protected UserEntity testUser1 = null
 
     protected String generateRandomString(int length) {
         String generatedString = RandomStringUtils.random(length, true, true)
@@ -51,6 +59,31 @@ class ProfileTestDataProvider extends Specification {
         companyRepository.save(testCompany)
 
         return testCompany
+    }
+
+
+    protected UserEntity createTestUser(IUserRepository userRepository) {
+
+        def userEntityOptional = userRepository.getById(testUserId1)
+        if(userEntityOptional.isPresent()){
+            testUser1 = userEntityOptional.get()
+            return testUser1
+        }
+        testUser1 = new UserEntity()
+        testUser1.passwordSalt = "passwordSalt"
+        testUser1.passwordHash = "passwordHash"
+        testUser1.birthDate = new Date()
+        testUser1.username = "username"
+        testUser1.companyId = testCompanyId
+        testUser1.firstName = "fname"
+        def identity = generateRandomString(15)
+        testUser1.identity = identity
+        testUser1.lastName = "lname"
+        testUser1.permission = 1
+        testUser1.id = testUserId1
+        userRepository.save(testUser1)
+
+        return testUser1
     }
 
     protected void createTestDepartments(IDepartmentRepository departmentRepository) {
---
+++ iflow/profile/src/test/groovy/com/pth/profile/test/repositories/UserDashboardMenuRepositorySpec.groovy
@@ -0,0 +1,162 @@
+package com.pth.profile.test.repositories
+
+import com.pth.profile.entities.UserDashboardMenuEntity
+import com.pth.profile.repositories.ICompanyRepository
+import com.pth.profile.repositories.IUserDashboardMenuRepository
+import com.pth.profile.repositories.IUserRepository
+import com.pth.profile.repositories.impl.CompanyRepository
+import com.pth.profile.repositories.impl.UserDashboardMenuRepository
+import com.pth.profile.repositories.impl.UserRepository
+import com.pth.profile.test.ProfileTestDataProvider
+import io.micronaut.context.ApplicationContext
+import io.micronaut.runtime.server.EmbeddedServer
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+
+class UserDashboardMenuRepositorySpec extends ProfileTestDataProvider {
+
+    @Shared
+    @AutoCleanup
+    private EmbeddedServer embeddedServer = ApplicationContext.run(EmbeddedServer, "testjpa")
+
+    private ICompanyRepository companyRepository
+
+    private IUserRepository userRepository
+
+    private IUserDashboardMenuRepository userDashboardMenuRepository
+
+    void setup() {
+        companyRepository = embeddedServer.applicationContext.createBean(CompanyRepository)
+        userDashboardMenuRepository = embeddedServer.applicationContext.createBean(UserDashboardMenuRepository)
+        userRepository = embeddedServer.applicationContext.createBean(UserRepository)
+
+        if(testCompany == null){
+            createTestCompany(companyRepository)
+        }
+        if(testUser1 == null){
+            createTestUser(userRepository)
+        }
+
+
+    }
+
+    void cleanup() {
+        def list = userDashboardMenuRepository.getAll()
+
+        for (UserDashboardMenuEntity entity : list) {
+            userDashboardMenuRepository.delete(entity)
+        }
+
+    }
+
+    void "verify new created userDashboardMenu can be queried by id and equals original userDashboardMenu"() {
+
+        given:
+            def userDashboardMenuEntity = new UserDashboardMenuEntity()
+            userDashboardMenuEntity.userId = testUserId1
+            userDashboardMenuEntity.menuId = "menu-1"
+            userDashboardMenuEntity.appId = "app-1"
+            userDashboardMenuEntity.rowIndex = 1
+            userDashboardMenuEntity.columnIndex = 1
+
+        when:
+            userDashboardMenuRepository.save(userDashboardMenuEntity)
+            def userDashboardMenuOptional = userDashboardMenuRepository.getById(userDashboardMenuEntity.id)
+
+        then:
+            userDashboardMenuOptional.isPresent()
+
+            verifyUserDashboardMenu(userDashboardMenuOptional.get(), userDashboardMenuEntity)
+
+    }
+
+    void "verify edited user can be queried by id and equals original user"() {
+
+        given:
+            def userDashboardMenuEntity = new UserDashboardMenuEntity()
+            userDashboardMenuEntity.userId = testUserId1
+            userDashboardMenuEntity.menuId = "menu-1"
+            userDashboardMenuEntity.appId = "app-1"
+            userDashboardMenuEntity.rowIndex = 1
+            userDashboardMenuEntity.columnIndex = 1
+            userDashboardMenuRepository.save(userDashboardMenuEntity)
+            def userDashboardMenuOptional = userDashboardMenuRepository.getById(userDashboardMenuEntity.id)
+            def userDashboardMenuSaved =  userDashboardMenuOptional.get()
+            userDashboardMenuSaved.menuId = "menu-edited"
+            userDashboardMenuSaved.appId = "app-edited"
+            userDashboardMenuSaved.rowIndex = 2
+            userDashboardMenuSaved.columnIndex = 2
+
+
+        when:
+            userDashboardMenuRepository.update(userDashboardMenuSaved)
+            userDashboardMenuOptional = userDashboardMenuRepository.getById(userDashboardMenuEntity.id)
+
+        then:
+            userDashboardMenuOptional.isPresent()
+
+            verifyUserDashboardMenu(userDashboardMenuOptional.get(), userDashboardMenuSaved)
+
+    }
+
+    void "verify deleted user can not be queried by id"() {
+
+        given:
+            def userDashboardMenuEntity = new UserDashboardMenuEntity()
+            userDashboardMenuEntity.userId = testUserId1
+            userDashboardMenuEntity.menuId = "menu-1"
+            userDashboardMenuEntity.appId = "app-1"
+            userDashboardMenuEntity.rowIndex = 1
+            userDashboardMenuEntity.columnIndex = 1
+            userDashboardMenuRepository.save(userDashboardMenuEntity)
+
+        when:
+            userDashboardMenuRepository.delete(userDashboardMenuEntity)
+            def userDashboardMenuOptional = userDashboardMenuRepository.getById(userDashboardMenuEntity.id)
+
+        then:
+            userDashboardMenuOptional.isPresent() == false
+
+    }
+
+    void "verify new created user list can be queried by get-all and equals original users"() {
+
+        given:
+        Map<UUID, UserDashboardMenuEntity> map = new HashMap<>()
+
+        for(int i=1; i<=10; i++){
+            def userDashboardMenuEntity = new UserDashboardMenuEntity()
+            userDashboardMenuEntity.userId = testUserId1
+            userDashboardMenuEntity.menuId = "menu-" + i
+            userDashboardMenuEntity.appId = "app-1"
+            userDashboardMenuEntity.rowIndex = i
+            userDashboardMenuEntity.columnIndex = i
+            userDashboardMenuRepository.save(userDashboardMenuEntity)
+            map.put(userDashboardMenuEntity.id, userDashboardMenuEntity)
+        }
+
+        when:
+
+            def userDashboardMenuList = userDashboardMenuRepository.getAll()
+
+        then:
+            userDashboardMenuList.isEmpty() == false
+            for(UserDashboardMenuEntity entity: userDashboardMenuList){
+                verifyUserDashboardMenu(map.get(entity.id), entity)
+            }
+
+
+    }
+
+
+
+    private void verifyUserDashboardMenu(UserDashboardMenuEntity testUserDashboardMenuEntity,
+                                         UserDashboardMenuEntity userDashboardMenuEntity) {
+        testUserDashboardMenuEntity.userId == userDashboardMenuEntity.userId
+        testUserDashboardMenuEntity.menuId == userDashboardMenuEntity.menuId
+        testUserDashboardMenuEntity.appId == userDashboardMenuEntity.appId
+        testUserDashboardMenuEntity.rowIndex == userDashboardMenuEntity.rowIndex
+        testUserDashboardMenuEntity.columnIndex == userDashboardMenuEntity.columnIndex
+    }
+
+}
